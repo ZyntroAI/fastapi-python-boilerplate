d82e116c3a0a8251906fc0a2dd1a748e777acd7f
ZyntroAI Team <283649378+zyntromedia@users.noreply.github.com>
2026-09-25 17:03:37 +0700

    Delete http_client_usage (1).py
    duplicate
    
    Signed-off-by: ZyntroAI Team <283649378+zyntromedia@users.noreply.github.com>

--- http_client_usage (1).py
+++
@@ -1,6 +0,0 @@
-from app.core.http_client import http_client
-
-@router.get("/external-data")
-async def get_external_data():
-    data = await fetch_external("https://api.example.com/data")
-    return data
